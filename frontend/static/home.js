03c376b6b71adc86bc0198a560b8fee8e93d61af
trentwiles <wilest7@gmail.com>
2024-10-28 10:12:49 -0400

    improvements to random property

--- frontend/static/home.js
+++ frontend/static/home.js
@@ -11,9 +11,19 @@ function mapSetup(id, lat, lon) {
   L.marker([lat, lon]).addTo(map).openPopup();
 }
 
+function convertEpochToReadableTime(epochMillis) {
+  const date = new Date(epochMillis);
+  return date.toLocaleString(); // This will use the default locale and format
+}
+
 function generateInfo(input) {
-  salePrice = `$${input["marketValue"].toLocaleString("en-US")}`;
-  return `Home with a market value of ${salePrice}`;
+  var salePrice = `$${input["marketValue"].toLocaleString("en-US")}`;
+  var type = input["landUse"].toLowerCase()
+  // times 1000 for javascript's built in date function
+  var epoch = convertEpochToReadableTime(input["scrapedAt"] * 1000);
+  return `Property with a market value of ${salePrice}, clasified as "${type}".<br />
+          Scraped at <time>${epoch}</time>
+          `;
 }
 
 $(document).ready(function () {
@@ -23,14 +33,17 @@ $(document).ready(function () {
       mapSetup("map1", 72, 42);
       $("#town1").text(data["results"][0]["streetAddressDetails"]["town"]);
       $("#state1").text(data["results"][0]["streetAddressDetails"]["state"]);
+      $("#content1").html(generateInfo(data["results"][0]))
 
       mapSetup("map2", 50, 39);
       $("#town2").text(data["results"][1]["streetAddressDetails"]["town"]);
       $("#state2").text(data["results"][1]["streetAddressDetails"]["state"]);
+      $("#content2").html(generateInfo(data["results"][1]))
 
       mapSetup("map3", 75, 40);
       $("#town3").text(data["results"][2]["streetAddressDetails"]["town"]);
       $("#state3").text(data["results"][2]["streetAddressDetails"]["state"]);
+      $("#content3").html(generateInfo(data["results"][2]))
     }
   });
 });
