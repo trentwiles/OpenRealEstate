48f5acbbb2e74e5708bcaefa9ba75f85b7349630
trentwiles <wilest7@gmail.com>
2024-11-01 16:15:51 -0400

    today's work

--- frontend/static/form.js
+++ frontend/static/form.js
@@ -1,6 +1,10 @@
 function handleForm() {
   var formData = new FormData(document.querySelector("form"));
-  if (formData.get("minPrice") != "" && formData.get("maxPrice") != "") {
+  if (
+    formData.get("minPrice") != "" &&
+    formData.get("maxPrice") != "" &&
+    formData.get("minPrice") != formData.get("maxPrice")
+  ) {
     var priceRange = formData.get("minPrice") + "-" + formData.get("maxPrice");
     formData.delete("minPrice");
     formData.delete("maxPrice");
@@ -19,28 +23,65 @@ function handleForm() {
   const formItemsToDelete = [];
 
   formData.forEach((value, key) => {
-    if (value === "" || value === null || value === undefined || (typeof value === "string" && value.trim() === "")) {
-        formItemsToDelete.push(key);
+    if (
+      value === "" ||
+      value === null ||
+      value === undefined ||
+      (typeof value === "string" && value.trim() === "")
+    ) {
+      formItemsToDelete.push(key);
     }
   });
 
   formItemsToDelete.forEach((key) => formData.delete(key));
 
   // form data is a crappy format, we should convert to JSON for the API
-  var preJSON = {}
+  var preJSON = {};
   formData.forEach((v, k) => {
-    preJSON[k] = v
-  })
+    preJSON[k] = v;
+  });
 
   return JSON.stringify(preJSON);
 }
 
+function reset() {
+  updatePriceRange()
+  $("#landUse").val("")
+  $("#owner").val("")
+  $("#town").val("")
+  $("#state").val("")
+  $("#startDate").val("")
+  $("#endDate").val("")
+  $("#minPriceSlider").val(500000) 
+  $("maxPriceSlider").val(500000)
+  updatePriceRange()
+}
+
 $(document).ready(function () {
+  updatePriceRange() 
   $("#submitButton").click(function (event) {
     event.preventDefault(); // Prevent form submission
-    const query = handleForm()
-    $.post({"url": "http://localhost:3000/search", contentType: 'application/json', dataType: 'json', data: query}, function(result){
-        console.log(result)
-      });
+
+    // Disable the button to prevent spamming
+    $(this).prop("disabled", true);
+
+    const query = handleForm();
+    $("#result").html("Please agree to terms of service before searching...");
+    $.post(
+      {
+        url: "http://localhost:3000/search",
+        contentType: "application/json",
+        dataType: "json",
+        data: query,
+      },
+      function (result) {
+        console.log(result);
+
+        $("#submitButton").prop("disabled", false);
+      }
+    ).fail(function () {
+      $("#submitButton").prop("disabled", false);
+      $("#result").html("An error occurred. Please try again.");
+    });
   });
 });
